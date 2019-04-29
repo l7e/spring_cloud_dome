05b34f69c4e1ffa10e2ccd4cdffff510de15dd99
l7e <7740280@qq.com>
2019-04-29 17:49:48 +0800

    根据服务ID获取IP和端口

--- consumer-server/src/main/java/com/like/controller/UserController.java
+++ consumer-server/src/main/java/com/like/controller/UserController.java
@@ -2,12 +2,17 @@ package com.like.controller;
 
 
 import com.like.pojo.User;
+import com.netflix.appinfo.InstanceInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -16,11 +21,19 @@ public class UserController
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/{id}")
     public User index(@PathVariable("id") Long id)
     {
-        String url = "http://127.0.0.1:8081/user/" + id;
-        return restTemplate.getForObject("http://127.0.0.1:8081/user/" + id, User.class);
+        //根据服务id获取服务的实例,注意这里是一个list,因为同名的服务可能有多个(集群)
+        List<ServiceInstance> instances = discoveryClient.getInstances("user-server");
+        ServiceInstance instance = instances.get(0);
+        //获取服务的ip和port拼接进行访问
+        String url = "http://" + instance.getHost() + ":" + instance.getPort() + "/user/";
+
+        return restTemplate.getForObject(url + id, User.class);
     }
 
 }
--- user-server/src/main/java/com/like/UserServer.java
+++ user-server/src/main/java/com/like/UserServer.java
@@ -3,12 +3,12 @@ package com.like;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("com.like.mapper")
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class UserServer
 {
     public static void main(String[] args)
